12d2c9d7bcdd563aea6c7deff75642d91180666b
vinaypuppal <puppalvinay@gmail.com>
2016-05-25 15:45:33 +0530

    Main App component

--- client/index.js
+++ client/index.js
@@ -1,21 +1,52 @@
 //import react
 import React,{ Component } from 'react';
 import { render } from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 //import stylesheet
 import css from './styles/main.sass';
 
-//create component
+//import componenst
+import Header from './components/header';
+import Footer from './components/footer';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyBie9yw838o6eOQawbj99e17FaJ5ksA5Y8';
+
+//create root component
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state ={
+			videos:[],
+			selectedVideo: null
+		};
+		this.fetchData('reactjs');
+	}
+	fetchData(term){
+		YTSearch({key:API_KEY,term},(videos)=>{
+			console.log(this);
+			this.setState({videos,selectedVideo:videos[0]});
+		});	
+	}
 	render() {
+		const debouncedFetchData = _.debounce((term)=>{this.fetchData(term)},300);
 		return (
 			<div>
-				<h1>Hello World</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum possimus similique, voluptas pariatur perspiciatis eveniet! Nemo at iusto alias tenetur sint laudantium, ullam est accusamus illo illum id commodi deserunt!</p>
+				<Header onSearchTermChange={debouncedFetchData}/>
+				<div className="wrapper">
+					<div className="container">
+						<VideoDetail video={this.state.selectedVideo}/>
+						<VideoList onVideoSelect={(selectedVideo)=>this.setState({selectedVideo})} videos={this.state.videos}/>
+					</div>
+				</div>
+				<Footer />
 			</div>
 		);
 	}
-}
+} 
 
 
 //render component
