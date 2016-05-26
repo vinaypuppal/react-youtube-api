5cb278956596286cf6a3666866752d8f52c6ce45
vinaypuppal <puppalvinay@gmail.com>
2016-05-26 18:07:38 +0530

    modify to support cloudnone port

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || process.env.app_port || 8080;
+var host = process.env.app_host || '127.0.0.1';
 
 app.use('/dist',express.static(path.join(__dirname, '/dist')));
 
@@ -17,5 +18,5 @@ app.listen(port, function(err) {
     return;
   }
 
-  console.log('Listening at http://127.0.0.1:'+port);
+  console.log('Web server running at http://' + host + ':' + port);
 });
